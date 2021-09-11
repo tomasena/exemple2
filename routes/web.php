e35f95ab8aff61cff231cac302d27f5df8d47482
tomasena <tomasena@gmail.com>
2021-09-11 10:21:47 +0200

    commit 1 dans git

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/product', function () {
+    return "Liste des produits";
+});
+
+Route::get('/product/{id}', function () {
+    return "Fiche du produit {id}";
+});
+
+Route::get('/cart', function () {
+    return "Panier";
+});
+
+ 
